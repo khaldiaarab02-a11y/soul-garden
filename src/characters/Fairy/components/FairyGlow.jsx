9df75a9b253c69913d24f20058f65cce1b269521
khaldiaarab02-a11y <khaldiaarab02@gmail.com>
2026-08-14 11:59:22 +0100

    Delete src directory

--- src/characters/Fairy/components/FairyGlow.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function FairyGlow({ children }) {
-  return (
-    <div className="fairy-glow" aria-hidden="true">
-      {children}
-    </div>
-  );
-}
